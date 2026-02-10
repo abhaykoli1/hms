5c9da81532b9d59f1673c2bb0e27e1a698607c51
abhaykoli1 <abhaykoli214@gmail.com>
2026-02-10 23:19:04 +0530

    C

--- core/database.py
+++ core/database.py
@@ -3,6 +3,6 @@ from mongoengine import connect
 def init_db():
     connect(
         db="hms_db",
-        host="mongodb+srv://infozodex_db_user:absolutions@data.yycywiw.mongodb.net/test2"
-        # host="mongodb+srv://avbigbuddy:nZ4ATPTwJjzYnm20@cluster0.wplpkxz.mongodb.net/hms_db"
+        # host="mongodb+srv://infozodex_db_user:absolutions@data.yycywiw.mongodb.net/test2"
+        host="mongodb+srv://avbigbuddy:nZ4ATPTwJjzYnm20@cluster0.wplpkxz.mongodb.net/hms_db1"
     )
