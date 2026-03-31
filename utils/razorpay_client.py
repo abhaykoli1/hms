263d481e2695e75c425e5e91354221da9e3a7faa
abhaykoli1 <abhaykoli214@gmail.com>
2026-04-01 00:42:19 +0530

    dfghjk

--- utils/razorpay_client.py
+++ utils/razorpay_client.py
@@ -2,5 +2,5 @@ import razorpay
 import os
 
 client = razorpay.Client(
-    auth=("rzp_live_SBbgiyIPp35rea", "DOXXoi562ExGGGHOL91gacUz")
+    auth=("rzp_live_SBbgiyIPp35rea", "SAeeakzGgBG3aRYXwefJv9ac")
 )
